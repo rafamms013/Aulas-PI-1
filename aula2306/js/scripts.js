5fa17d30e79742134479134d652ff8b54cd095cc
Rafael Montenegro M. da Silva <88899571+rafamms013@users.noreply.github.com>
2022-10-05 13:32:20 -0300

    Add files via upload

---
+++ aula2306/js/scripts.js
@@ -0,0 +1,18 @@
+var nomes = new Array();
+
+
+function CriarArray(){
+    
+
+    novoNome = document.getElementById('txtNome').value;
+
+    nomes.push(novoNome);
+    alert(nomes[0]);
+    alert(nomes.length);
+    
+    
+    
+   
+
+
+}
